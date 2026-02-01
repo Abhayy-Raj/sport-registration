24f47a6157ec411193a8fc206e00d16fc73285d5
Abhayy-Raj <abhayrajd144@gmail.com>
2026-02-01 15:39:15 +0530

    changed the api link app.js

--- frontend/app.js
+++ frontend/app.js
@@ -12,7 +12,7 @@ document.querySelector("form").addEventListener("submit", async (e) => {
         year: document.getElementById("year").value
     };
 
-    const response = await fetch("https://sport-registration-backend.onrender.com/register", {
+    const response = await fetch("https://sport-registration.onrender.com/register", {
         method: "POST",
         headers: {
             "Content-Type": "application/json"
