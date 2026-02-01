de8fef76d8c3067a438522c38a52c13eaaeee3a7
Abhayy-Raj <abhayrajd144@gmail.com>
2026-02-01 18:50:58 +0530

    Server error resolved

--- backend/server.js
+++ backend/server.js
@@ -47,11 +47,12 @@ app.post("/register", async (req, res) => {
     }
 
     await pool.query(
-      `INSERT INTO users
-       (name, reg_id, phone, section, year)
-       VALUES ($1, $2, $3, $4, $5)`,
-      [name, registrationId, phone, section, year]
-    );
+  `INSERT INTO users
+   (name, reg_id, phone, section, study_year)
+   VALUES ($1, $2, $3, $4, $5)`,
+  [name, registrationId, phone, section, year]
+);
+
 
     res.status(200).json({ message: "Registration successful" });
   } catch (error) {
