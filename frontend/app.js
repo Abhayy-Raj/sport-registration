ca025a6f522c43f9024186c38c14494f5b16b16e
Abhayy-Raj <abhayrajd144@gmail.com>
2026-02-01 09:33:06 +0530

    Removed node modules from repo

---
+++ frontend/app.js
@@ -0,0 +1,25 @@
+console.log("JS LOADED");
+
+document.querySelector("form").addEventListener("submit", async (e) => {
+
+    e.preventDefault();
+
+    const data = {
+        name: document.getElementById("name").value,
+        regId: document.getElementById("reg-id").value,
+        phone: document.getElementById("phone").value,
+        section: document.getElementById("section").value,
+        year: document.getElementById("year").value
+    };
+
+    const response = await fetch("http://localhost:5000/register", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(data)
+    });
+
+    const result = await response.json();
+    alert(result.message);
+});
