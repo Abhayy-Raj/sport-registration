4b857b1b6730831fe248a53f07e1625a9247253a
Abhayy-Raj <abhayrajd144@gmail.com>
2026-02-01 14:57:23 +0530

    Fix database connection to use Neon DATABASE_URL

--- backend/db.js
+++ backend/db.js
@@ -1,15 +1,14 @@
 const { Pool } = require("pg");
 
 const pool = new Pool({
-    user: "postgres",
-    host: "localhost",
-    database: "sport_registration",
-    password: "112233",
-    port: 5432,
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 pool.connect()
-    .then(() => console.log("PostgreSQL connected"))
-    .catch(err => console.error("Connection error", err));
+  .then(() => console.log("Connected to Neon database"))
+  .catch(err => console.error("Database connection error:", err));
 
 module.exports = pool;
